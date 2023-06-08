a062a86b945ec98b6d349095d0b00daab24fffaf
Jesús Bintaned <jesus@10labs.es>
2023-06-08 16:55:32 +0200

    Añadidos inserts

--- pre-processing-scripts/create-table.sql
+++ pre-processing-scripts/create-table.sql
@@ -1,18 +1,3 @@
-create schema malware;
-
-create table malware.malware_data_filtered
-(
-    country text,
-    datetime text,
-    delivery_method text,
-    file_size bigint,
-    file_type text,
-    malware_type text,
-    month integer,
-    num_detections integer,
-    num_week integer,
-    sha1_hash text not null
-        constraint "malware-data-filtered_pkey"
-            primary key,
-    year integer
-);
+version https://git-lfs.github.com/spec/v1
+oid sha256:74e84f68493d1541cc74d0bad606f78b8835d70c48fe27e30f1e3fd97f357fbb
+size 386
